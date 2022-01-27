b0d514ae183c97afb9726440b2558224ae131b65
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-01-28 00:41:48 +0330

    updated preview to fix the background color issue while resizing in src-components

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -7,7 +7,9 @@ interface PreviewProps {
 
 const html = `
 <html>
-  <head></head>
+  <head>
+    <style>html { background-color: white; }</style>
+  </head>
   <body>
     <div id="root"></div>
     <script>
