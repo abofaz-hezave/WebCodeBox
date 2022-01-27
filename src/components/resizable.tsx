593baed7f0f1f8b669ed782d59f7aa478aeab1e8
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-01-28 00:33:40 +0330

    updated resizable component to handle horizontal mode in src-components

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,27 +1,33 @@
 import './resizable.css';
-import { useState } from 'react';
-import { ResizableBox } from 'react-resizable';
-import Preview from './preview';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  // const [input, setInput] = useState('');
-  // const [code, setCode] = useState('');
+  let resizableBoxProp: ResizableBoxProps;
 
-  return (
-    <ResizableBox
-      minConstraints={[Infinity, window.innerHeight * 0.1]}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      height={300}
-      width={Infinity}
-      resizeHandles={['s']}
-    >
-      {children}
-    </ResizableBox>
-  );
+  if (direction === 'horizontal') {
+    resizableBoxProp = {
+      className: 'resize-horizontal',
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableBoxProp = {
+      minConstraints: [Infinity, window.innerHeight * 0.1],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      height: window.innerHeight * 0.6,
+      width: Infinity,
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizableBoxProp}>{children}</ResizableBox>;
 };
 
 export default Resizable;
