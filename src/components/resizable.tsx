e1b1cc968970adea9eed869fa50e23d2da05ea43
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-01-28 23:41:43 +0330

    updated resizable component to make it responsive while the size of window is changing in src-components

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
@@ -7,21 +8,49 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableBoxProp: ResizableBoxProps;
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [boxWidth, setBoxWidth] = useState(window.innerWidth * 0.75);
+
+  useEffect(() => {
+    let resizeTimer: any;
+    const resizeListener = () => {
+      if (resizeTimer) {
+        clearTimeout(resizeTimer);
+      }
+      setTimeout(() => {
+        setInnerWidth(window.innerWidth);
+        setInnerHeight(window.innerHeight);
+      }, 100);
+      if (window.innerWidth * 0.75 < boxWidth) {
+        setBoxWidth(window.innerWidth * 0.75);
+      }
+    };
+
+    window.addEventListener('resize', resizeListener);
+
+    return () => {
+      window.removeEventListener('resize', resizeListener);
+    };
+  }, []);
 
   if (direction === 'horizontal') {
     resizableBoxProp = {
       className: 'resize-horizontal',
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width: boxWidth,
       resizeHandles: ['e'],
+      onResizeStop: (event, data) => {
+        setBoxWidth(data?.size?.width);
+      },
     };
   } else {
     resizableBoxProp = {
-      minConstraints: [Infinity, window.innerHeight * 0.1],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
-      height: window.innerHeight * 0.6,
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, innerHeight * 0.9],
+      height: 300,
       width: Infinity,
       resizeHandles: ['s'],
     };
