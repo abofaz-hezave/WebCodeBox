9b74ab6d18939794b147e612bd28a80c6e9c480d
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-01-28 23:58:58 +0330

    updated preview to add a short delay between updating srcdoc and sending new code to the iframe in src-components

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -32,7 +32,9 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, '*');
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    }, 50);
   }, [code]);
 
   return (
