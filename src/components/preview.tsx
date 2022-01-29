c11c1da7f6f7f2fe73a2c77e527383a19c553347
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-01-29 23:41:23 +0330

    updated preview to handle if the entered code has any error in src-components

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -13,13 +13,22 @@ const html = `
   <body>
     <div id="root"></div>
     <script>
+    const handleError = (err) => {
+      const root = document.querySelector('#root');
+      root.innerHTML =
+        '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+      console.error(err);
+    };
+    window.addEventListener('error', (event) => {
+      event.preventDefault();
+      handleError(event.error);
+    });
+    
       window.addEventListener('message', (event) => {
         try {
           eval(event.data);
         } catch (err) {
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-          console.error(err);
+          handleError(err)
         }
       }, false);
     </script>
