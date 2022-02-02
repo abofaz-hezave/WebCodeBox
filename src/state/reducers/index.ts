b6d10ec7769b8b5b9503fce1ee95a388bfd0cb83
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-02-03 00:22:50 +0330

    created state folder and neccessary files for redux in src-state

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import cellsReducer from './cellsReducer';
+// import bundlesReducer from "./bundlesReducer";
+
+const reducers = combineReducers({
+  cells: cellsReducer,
+});
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
