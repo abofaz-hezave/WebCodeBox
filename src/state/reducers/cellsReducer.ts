20d6507b8873b54e93b846aa25f66cc142d96ab3
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-02-04 22:56:32 +0330

    used immer to smplify the CellsReducer in src-state

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,6 +1,8 @@
+import produce from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
+import { randomId } from '../../utils';
 
 interface CellsState {
   loading: boolean;
@@ -18,30 +20,50 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
-  switch (action.type) {
-    case ActionType.MOVE_CELL:
-      return state;
-    case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.UPDATE_CELL:
-      const { id, content } = action.payload;
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: { ...state.data[id], content },
-        },
-      };
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
+const reducer = (state: CellsState = initialState, action: Action) =>
+  produce(state, (draft) => {
+    switch (action.type) {
+      case ActionType.MOVE_CELL:
+        const { direction, id } = action.payload;
+        const index = draft.order.findIndex((item) => item === id);
+        const targetIndex = direction === 'up' ? index - 1 : index + 1;
+        if (targetIndex < 0) return draft;
+        if (targetIndex > draft?.order?.length - 1) return draft;
+        draft.order[index] = draft?.order?.[targetIndex];
+        draft.order[targetIndex] = id;
+        return draft;
 
-    default:
-      return state;
-  }
-};
+      case ActionType.DELETE_CELL:
+        delete draft.data[action.payload];
+        draft.order = draft.order.filter((item) => item !== action.payload);
+        return draft;
+
+      case ActionType.UPDATE_CELL:
+        draft.data[action.payload.id].content = action.payload.content;
+        return draft;
+
+      case ActionType.INSERT_CELL_BEFORE:
+        const cell: Cell = {
+          content: '',
+          type: action.payload.type,
+          id: randomId(),
+        };
+        const foundIndex = draft.order.findIndex(
+          (item) => item === action.payload.id
+        );
+
+        draft.data[cell.id] = cell;
+        if (foundIndex < 0) {
+          draft.order.push(cell.id);
+        } else {
+          draft.order.splice(foundIndex, 0, cell.id);
+        }
+
+        return draft;
+
+      default:
+        return draft;
+    }
+  });
 
 export default reducer;
