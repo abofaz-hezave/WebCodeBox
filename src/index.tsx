6097737402068ba994938c74b38518731426722a
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-02-06 22:30:04 +0330

    updated the main component to render the CellList in src

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
-// import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <TextEditor />
+      <CellList />
     </Provider>
   );
 };
