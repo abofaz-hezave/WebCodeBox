f5c71bc7e4bdfac9cdc0cfc8e09df5601c15c623
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-02-08 20:45:02 +0330

    created an ActionBar for cell-items in src-components

---
+++ src/components/action-bar.tsx
@@ -0,0 +1,42 @@
+import './action-bar.css';
+import { useActions } from '../hooks/use-actions';
+// import CellListItem from './cell-list-item';
+
+interface ActionBarProps {
+  id: string;
+}
+
+const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
+  const { moveCell, deleteCell } = useActions();
+
+  return (
+    <div className="action-bar">
+      <button
+        className="button is-primary is-small"
+        onClick={() => moveCell(id, 'up')}
+      >
+        <span className="icon">
+          <i className="fas fa-arrow-up" />
+        </span>
+      </button>
+      <button
+        className="button is-primary is-small"
+        onClick={() => moveCell(id, 'down')}
+      >
+        <span className="icon">
+          <i className="fas fa-arrow-down" />
+        </span>
+      </button>
+      <button
+        className="button is-primary is-small"
+        onClick={() => deleteCell(id)}
+      >
+        <span className="icon">
+          <i className="fas fa-times" />
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default ActionBar;
--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,6 +1,8 @@
+import './cell-list-item.css';
 import { Cell } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
+import ActionBar from './action-bar';
 
 interface CellListItemProps {
   cell: Cell;
@@ -14,7 +16,12 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
     cellItem = <TextEditor cell={cell} />;
   }
 
-  return <div>{cellItem}</div>;
+  return (
+    <div className="cell-list-item">
+      {cellItem}
+      <ActionBar id={cell.id} />
+    </div>
+  );
 };
 
 export default CellListItem;
