1b75625368ae51f8d3c02063f892c769d1d93861
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-02-18 21:52:04 +0330

    updated useActions to use useMemo to create bindActionCreators only once in src-hooks

--- src/hooks/use-actions.ts
+++ src/hooks/use-actions.ts
@@ -1,8 +1,10 @@
+import { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state';
 
 export const useActions = () => {
   const dispatch = useDispatch();
-  return bindActionCreators(actionCreators, dispatch);
+
+  return useMemo(() => bindActionCreators(actionCreators, dispatch), []);
 };
