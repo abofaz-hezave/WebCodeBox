80de0aa8e7347ea21aabfaaad1b037b0ebefcfa9
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-02-26 23:40:53 +0330

    updated  CodeCell to avoid executing the show functions of previous cells in src-compoents

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -17,27 +17,30 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
-    const cumulativeCode = [
-      `
-      import Cell_React from 'react';
-      import Cell_ReactDOM from 'react-dom';
+    const isCodeCell = (selectedCell: Cell) => selectedCell.type === 'code';
+    const isCurrentCell = (selectedCell: Cell) => selectedCell.id === cell.id;
 
-      const show = (value) => {
-        const root = document.querySelector('#root');
-        if (typeof value !== 'object') return (root.innerHTML = value);
-        if (value.$$typeof && value.props) return Cell_ReactDOM.render(value, root);
-        return (root.innerHTML = JSON.stringify(value));
-      };
-      `,
-    ];
+    const actionShow = `
+    import Cell_React from 'react';
+    import Cell_ReactDOM from 'react-dom';
+
+     show = (value) => {
+      const root = document.querySelector('#root');
+      if (typeof value !== 'object') return (root.innerHTML = value);
+      if (value.$$typeof && value.props) return Cell_ReactDOM.render(value, root);
+      return (root.innerHTML = JSON.stringify(value));
+    };
+    `;
+    const cumulativeCode = ['let show = (value) => {}'];
 
     for (let cumulatingCell of orderedCells) {
-      if (cumulatingCell.type === 'code') {
+      if (isCodeCell(cumulatingCell))
         cumulativeCode.push(cumulatingCell.content);
-      }
-      if (cumulatingCell.id === cell.id) {
-        break;
-      }
+
+      if (isCodeCell(cumulatingCell) && isCurrentCell(cumulatingCell))
+        cumulativeCode.push(actionShow);
+
+      if (isCurrentCell(cumulatingCell)) break;
     }
     return cumulativeCode;
   });
