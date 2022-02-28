8705d4e87719ccd197ac853e222bc43b69ebf8f8
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-02-28 21:33:38 +0330

    created cell-list.css file and added some horizontal margi to cellList in src-components

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import './cell-list.css';
 import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
@@ -17,7 +18,7 @@ const CellList: React.FC = () => {
     ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={!cells?.length} previousCellId={null} />
       {cellsMap()}
     </div>
